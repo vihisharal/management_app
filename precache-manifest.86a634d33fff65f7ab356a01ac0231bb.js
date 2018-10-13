a0964033d96af340dafb9b83f70b8c73e7d3c035
vihisharal <vihisharal826@gmail.com>
2018-10-13 23:51:15 +0530

    Updates

---
+++ precache-manifest.86a634d33fff65f7ab356a01ac0231bb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/management_app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "25cfeb61c3e8423277d5",
+    "url": "/management_app/static/css/main.f5741332.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/management_app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "4c5e89080e0e48392381",
+    "url": "/management_app/static/js/1.4c5e8908.chunk.js"
+  },
+  {
+    "revision": "c5b6d670e3b9572fd070",
+    "url": "/management_app/static/js/runtime~main.c5b6d670.js"
+  },
+  {
+    "revision": "25cfeb61c3e8423277d5",
+    "url": "/management_app/static/js/main.25cfeb61.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/management_app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/management_app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/management_app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "4c5e89080e0e48392381",
+    "url": "/management_app/static/css/1.3c90a55b.chunk.css"
+  },
+  {
+    "revision": "770cb9f4f79a3f33d9ea3ebe470673af",
+    "url": "/management_app/index.html"
+  }
+];
